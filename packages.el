48489b610a8238e7a00d9612adda709177f2b9bc
zhuoyikang <zhuoyikang@gmail.com>
2016-11-05 22:53:08 +0800

    up

--- packages.el
+++ packages.el
@@ -13,11 +13,14 @@
   '(
     company
     erlang
+    alchemist
     ;;distel
     ))
 
 (defun zyk-erlang/post-init-company ()
   (add-hook 'erlang-mode-hook 'company-mode))
+
+(set-default 'indent-tabs-mode nil)
 
 
 (defun zyk-erlang/init-erlang ()
@@ -35,6 +38,7 @@
                 (lambda ()
                   (setq mode-name "Erlang")
                   (flycheck-mode)
+                  (setq indent-tabs-mode nil)
                   (modify-syntax-entry ?_ "w") ;;让this_good连成一个单词处理
                   ;; when starting an Erlang shell in Emacs, with a custom node name
                   (setq-default indent-tabs-mode nil)
@@ -59,14 +63,136 @@
 (setq flycheck-erlang-include-path (list "../include"
                                          "../../include"
                                          "../../../include"
+                                         "../../../../include"
+                                         "../../../../../include"
                                          "../deps/rabbit_common/include"
                                          "../deps/lager/include"
-                                         "/Users/zhuoyikang/source/vcity/all/apps/proto/include"
+                                         "/Users/zhuoyikang/source/slash/apps/proto/include"
+
+                                         "/Users/zhuoyikang/source/services/ejabberd_install/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/deps/esip/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/deps/fast_xml/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/deps/lager/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/deps/p1_xmlrpc/include"
+                                         "/Users/zhuoyikang/source/services/ejabberd/deps/stun/include"
+
+                                         "/Users/zhuoyikang/source/vcity/apps/proto/include"
                                          "../deps/proto/include"))
 
 (setq flycheck-erlang-library-path (list "ebin" "../ebin"  "../../ebin"  "../../../ebin" "../deps/*/ebin" ))
 
+(setenv "ERL_LIBS" "/Users/zhuoyikang/source/vcity/deps")
 
-(setenv "ERL_LIBS" "/Users/zhuoyikang/source/vcity/all/deps")
+(defvar rebar-directory "gl_online"
+  "匹配这个正则表达式的路径将会认为是工作文件夹,工作文件夹下的erlang文件被保存时触发自动编译.")
 
-;;(setq erlang-indent-level 4)
+
+(setq rebar-directory  (list "vcity" "slash"))
+
+
+(setq beam_sync_list "/Users/zhuoyikang/Source/gl2/gl1x/")
+(setq beamHash (make-hash-table :test 'equal))
+
+
+;; 保存erlang文件时直接编译.
+(defun rebar-compile-source ()
+  "编译当前缓冲区的文件，在文件保存后执行."
+  (let ((buffer-name (buffer-file-name (current-buffer)))
+        (match-pos nil)(full-path nil)
+        (compile-cmd nil)(list-directory rebar-directory)
+        (match-directory nil))
+    (if (string-equal "erl" (file-name-extension buffer-name))
+        (progn
+          (while (and  (car list-directory)  (equal nil match-directory))
+            (if (string-match (car list-directory) buffer-name)
+                (setq  match-directory (car list-directory)))
+            (setq list-directory (cdr list-directory)))
+          (if (and match-directory (string-match match-directory buffer-name))
+              (progn
+                (setq match-pos (string-match match-directory buffer-name))
+                (setq full-path (substring buffer-name 0 match-pos))
+                (setq compile-cmd  (concat
+                                    "erlc  -pa /Users/zhuoyikang/source/vcity/deps/lager/ebin "
+                                    " +'{parse_transform, lager_transform}' "
+                                    "-o " full-path
+                                    match-directory "/ebin/" " -Wall "
+                                    "-I " full-path match-directory "/include "
+                                    "-I " full-path match-directory "/apps/proto/include "
+                                    "-pa " full-path match-directory "/ebin "
+                                    buffer-name
+                                    ))
+                (shell-command compile-cmd "*Messages")
+                (message compile-cmd  "*Messages")
+                ;; (beam_hash_cp_key (concat full-path match-directory "/ebin/"
+                ;;                           (replace-regexp-in-string ".erl" ".beam" (buffer-name))
+                ;;                           ))
+                )
+            )
+          )
+      )
+    )
+  )
+
+
+(defun read-lines (filePath)
+  "Return a list of lines of a file at filePath."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (split-string (buffer-string) "\n" t)))
+
+
+;; 循环遍历copy
+(defun beam_hash_cp_key(key)
+  ;;(message key)
+  (let ((value (gethash key beamHash)))
+    (while (car value)
+      (let ((v1 (car value)))
+        (progn ()
+               (setq compile-cmd  (concat  "cp " key " " v1))
+               (shell-command compile-cmd "*Messages")
+               (message compile-cmd "*Messages")
+               )
+        )
+      (setq value (cdr value))
+      )
+    )
+  )
+
+
+
+(defun load_beam_hash ()
+  (let ((list (read-lines (concat beam_sync_list "scripts/.sync.sh")))
+        )
+    (while (car list)
+      ;;(message (car list) "*Messages")
+      (let ((scp (split-string  (car list)))
+            )
+        (if (> (length (list scp)) 0)
+            (progn
+              ;;(message (concat beam_sync_list (nth 1 scp)) "*Messages")
+              (puthash (concat beam_sync_list (nth 1 scp))
+                       (push (concat beam_sync_list (nth 2 scp)) (gethash (concat beam_sync_list (nth 1 scp)) beamHash))
+                       beamHash)
+              )
+          )
+        )
+      (setq list (cdr list)))
+    )
+  )
+
+
+(add-hook 'after-save-hook 'rebar-compile-source)
+
+
+
+(defun ops()
+  (interactive)
+  (progn
+    (find-file-existing "~/source")
+    )
+  )
+
+
+
+(setq erlang-indent-level 4)
